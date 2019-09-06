dd81df00d86016dbebd7c7b93a0975baafb71053
Hamidou Diallo <dio.hamidou@gmail.com>
2019-09-06 07:37:17 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
+const getFirstSelector = selector =>{
+  return document.querySelector(selector)
+}
+
+
+const nestedTarget =() =>{
+  return document.querySelector('.target')
+}
+
+const increaseRankBy = n =>{
+  const rankedList = document.querySelector('.ranked-list')
+  let incrementRanks = rankedList.forEach(item =>{
+    parseInt(item)+=n
+  });
+  return incrementRanks
+}
 
